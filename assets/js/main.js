eae4cea99e4920cf5c5818dfa0531dce9a4a88e5
stephanusveri <stephanusveri29@gmail.com>
2022-08-17 15:41:29 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,23 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+});
+
+sr.reveal('.home__title, .popular__container, .features__img, .featured__filters')
+sr.reveal('.home__title', {delay: 300})
+sr.reveal('.home__elec', {delay: 400})
+sr.reveal('.home__img', {delay: 600})
+sr.reveal('.home__car-data', {delay: 700, interval: 100, origin: 'bottom'})
+sr.reveal('.home__button', {delay: 1000, origin: 'bottom'})
+sr.reveal('.about__group, .offer__data', {origin: 'left'})
+sr.reveal('.about__data, .offer__img', {origin: 'right'})
+sr.reveal('.features__map', {delay: 500, origin: 'bottom'})
+sr.reveal('.features__card', {interval: 300})
+sr.reveal('.featured__card, .logos__content, .footer__container', {interval: 100})
+
